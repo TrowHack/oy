76cb7793477197cd5bde031a33d98a10fd553ed4
TrowHack <pv1000gg@gmail.com>
2025-10-31 17:44:47 +0300

    Update and rename .lua to FTAP.lua

--- .lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TrowHack/oy/refs/heads/main/" .. game.PlaceId))()
---
+++ FTAP.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TrowHack/scripts/refs/heads/roblox/" .. game.PlaceId))()
